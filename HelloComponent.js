ef9f44e9f40e4a1210036b813db78496c4befefa
Dod-Annie <936166900@qq.com>
2017-09-02 23:23:11 +0800

    add

---
+++ HelloComponent.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class HelloComponent extends React.Component {
+  render() {
+    return (
+      <Text style={{fontSize:20,backgroundColor:'red'}}>Hello</Text>
+    )
+  }
+}
+
