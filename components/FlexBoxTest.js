a16e5b790ef5e43125eb279459456b754afb0d25
Dod-Annie <936166900@qq.com>
2017-09-04 09:55:49 +0800

    add

--- components/FlexBoxTest.js
+++ components/FlexBoxTest.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default class FlexBoxTest extends React.Component {
   render() {
     return (
-      <View style={{ flexDirection:'row-reverse',backgroundColor: 'darkgray',marginTop:20}}>
+      <View style={{ flexDirection: 'row-reverse', backgroundColor: 'darkgray', marginTop: 20 }}>
         <View style={{ width: 40, height: 40, backgroundColor: 'darkcyan', margin: 5 }}>
           <Text style={{ fontSize: 16 }}>1</Text>
         </View>
